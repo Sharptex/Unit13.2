dc6c55fd1b47be742b823677eb17b5b29130097b
Alex <AleKsusha@DESKTOP-G48TPNV>
2021-05-29 00:07:52 +0300

    Add project files.

---
+++ Unit13.2/ArrayExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit13._2
+{
+    public static class ArrayExtensions
+    {
+        public static void ShowTop(this string[] words, int max)
+        {
+            Console.WriteLine($"{max} слов наиболее часто встречающиеся в тексте: ");
+
+            var freqeuncyDictionary = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (freqeuncyDictionary.ContainsKey(word))
+                {
+                    freqeuncyDictionary[word]++;
+                }
+                else
+                {
+                    freqeuncyDictionary.Add(word, 1);
+                }
+            }
+
+            int counter = 0;
+            foreach (var item in freqeuncyDictionary.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} раз");
+                counter++;
+                if (counter > max) break;
+            }
+        }
+    }
+}
---
+++ Unit13.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit13._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Text1.txt");
+            var words = LoadData(textFile);
+
+            words.ShowTop(10);
+
+            Console.ReadKey();
+        }
+
+        public static string[] LoadData(string path)
+        {
+            var result = new List<string>();
+
+            var text = File.ReadAllText(path);
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            char[] separators = new[] { ' ' };
+            var words = noPunctuationText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            return words;
+        }
+    }
+}
